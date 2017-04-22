7f4b5225102c7b358a01b16e6a6b79dcb6d518e5
Matthias Beyer <mail@beyermatthias.de>
2017-04-22 16:21:29 +0200

    fixup! The binary lives in the "deps" folder

--- lib/thermite/tasks.rb
+++ lib/thermite/tasks.rb
@@ -123,7 +123,7 @@ module Thermite
         if cargo
           profile = ENV.fetch('CARGO_PROFILE', 'release')
           run_cargo_rustc(profile)
-          FileUtils.cp(config.cargo_target_path(profile, "deps", config.shared_library),
+          FileUtils.cp(config.cargo_target_path(profile, 'deps', config.shared_library),
                        config.ruby_path('lib'))
         elsif !download_binary_from_custom_uri && !download_binary_from_github_release
           inform_user_about_cargo
